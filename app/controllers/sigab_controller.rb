5746df9abda5d71acaa85d1f4c820cb7ce9a1b28
davipatury <davi.patury@yahoo.com.br>
2023-01-26 14:04:36 -0300

    .

---
+++ app/controllers/sigab_controller.rb
@@ -0,0 +1,10 @@
+class SigabController < ActionController::Base
+  def index
+
+  end
+
+  def fluxo
+    # requisição api do fluxograma
+    # @fluxo = request ...
+  end
+end
---
+++ app/helpers/sigab_helper.rb
@@ -0,0 +1,2 @@
+module SigabHelper
+end
